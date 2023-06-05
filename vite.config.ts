a9689a7fd9ccaef72de45e5dfa9e894152d640b3
evandaru <fznhsym@gmail.com>
2023-06-05 09:50:50 +0700

    ikan

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: '',
+        target: 'https://script.google.com/macros/s/AKfycbzqW-YpwEcTSS8UI2Ge11XkZXv57KTyu45uDw9nLGbh7jBNPFnCqoGkSxYOXx25HsuedA/exec?',
         changeOrigin: true,
         headers: {
-          // Referer: 'http://localhost:5173',
+          Referer: 'https://quranfirstbeta.vercel.app',
           "Access-Control-Allow-Origin": "*"
         }
       }
